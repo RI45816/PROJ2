b69a5f76ed938676dea1eee4a4fbc9ba4a7229ef
RI45816 <uu3@umbc.edu>
2016-11-30 18:25:00 -0500

    first commit

---
+++ proj2.py
@@ -0,0 +1,20 @@
+# File: proj1.py
+# Author: Uzoma Uwanamodo
+# Date: 11/30/2016
+# Section: 05
+# E-mail: uu3@umbc.edu
+# Description:
+# A Program that finds paths through mazes
+# Collaboration:
+# Collaboration was not allowed on this assignment
+
+
+
+
+
+# I was not able to complete the win/lose functionality
+
+# The user's menu options
+
+if __name__ == "__main__":
+    print("Hello World")
