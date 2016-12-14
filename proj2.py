8bf813ec85e4fe4de78070f5aa701eaad17510ff
RI45816 <uu3@umbc.edu>
2016-12-13 21:45:15 -0500

    @U3VS3@

--- proj2.py
+++ proj2.py
@@ -9,72 +9,7 @@
 # Collaboration was not allowed on this assignment
 
 
-################## DEBUGGING #########################
-
-
 import sys
-import logging
-logging.basicConfig(level=logging.DEBUG, format='\n%(asctime)s - %(levelname)s :\n%(message)s\n')
-import traceback
-import inspect
-
-
-def getParentheticalText(text,i=-1,j=0):
-    
-    
-    if "," in text:
-        if "(" in text:
-            k,l = text.index("("),text.index(",")
-            if l<k:
-                print(k,l)
-                return [text[:l]]+getParentheticalText(text[l+1:])
-        else:
-            return text.split(",")
-    
-    if not text:
-        return []
-    
-    
-    if "(" not in text[i+1:]:
-#        if not text[j:]:
-        if ")" not in text[i+1:]:
-                return [text]
-        return [text[:j+1]]+getParentheticalText(text[j+1:])
-#        else
-    k,l = text.index("(",i+1),text.index(")",j+1)
-    
-    if "(" not in text[i+1:] or (k >= j and j):
-            return text[:j+1]
-    
-    
-    
-    return getParentheticalText(text,k,l)
-
-
-def func(*var):
-#    print(inspect.stack()[-2][4])
-    code = ([i[3] for i in traceback.extract_stack()])
-    code = list(filter(lambda x:"debug" in x,code))[0]
-#    print(code)
-    start = 13 if "vars()," in code else 6
-    varNames = getParentheticalText(code[start:-1])
-#    varNames = getParentheticalText()
-    return ["%s: %s" % (varNames[i],var[i]) for i in range(len(var))]
-    
-
- # debug
-def debug(args,*var):
-    if not "vars()" in ",".join([i for i in [i[3] for i in traceback.extract_stack()][:-1]]):
-        var = list(var)
-        var = [args] + var
-        args=func(*var)
-    elif var:
-        args = ["%s: %s" % (i,args[i]) for i in args]
-        args += var
-#    print(args)
-    return logging.debug("\n".join(["%s"] * len(args)),*args)
-
-################## DEBUGGING #########################
 
 
 WALL = 1
@@ -117,7 +52,6 @@ def readMaze(filename):
 # Input: maze: the maze, currentCoordinates: the coordinates of the pathfinder, the number indicating the previous direction, endPoint: the coordinates of the endpoint
 def searchMaze(maze,currentCoordinates,prevDirection,endPoint):
     
-    
     ## Get current square
     # Set coordinate variables
     i,j=currentCoordinates
@@ -137,15 +71,14 @@ def searchMaze(maze,currentCoordinates,prevDirection,endPoint):
         return [str(currentCoordinates)]
     
     # Get the direction to check, using the previous direction as the last one to check for backtracking
-    directionsToCheck = (list(filter(lambda z:z!=prevDirection,range(4))) + ([prevDirection] if prevDirection < -1 else []))
+    directionsToCheck = (list(filter(lambda z:z!=prevDirection,range(4))))
+    
     for x in directionsToCheck:
         if not current[x]:
-#            debug(currentCoordinates,endPoint,x,current,prevDirection,directionsToCheck)   
-            return [str(currentCoordinates)] +  searchMaze(maze, [(i,j+1),(i+1,j),(i,j-1),(i-1,j)][x],((x + 2) % 4),endPoint)
-            
-    print("No solution found")
+            hello = [str(currentCoordinates)] + searchMaze(maze, [(i,j+1),(i+1,j),(i,j-1),(i-1,j)][x],((x + 2) % 4),endPoint)
+            return(hello)
+#    return searchMaze(maze, (i,j),((x + 2) % 4),endPoint)
     return []
-    
     
 def main():
     
